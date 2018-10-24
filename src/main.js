c05531453e9ac582e06c9f76028887a8d832c1f5
levi85 <joshua.zhangzy@gmail.com>
2018-10-24 23:58:17 +0800

    Added Vuetify via CLI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(router)
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
